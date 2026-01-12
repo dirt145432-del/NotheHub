6f9a3ada59883ffa4ad5441454acbf4ec27a26d7
dirt145432-del <ayse.tms21@gmail.com>
2026-01-12 09:45:50 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,34 +5,37 @@ local Window = Library.CreateLib("Nothe Hub", "DarkTheme")
 local Tab1 = Window:NewTab("Oyun Scriptleri")
 local Section1 = Tab1:NewSection("Popüler Oyunlar")
 
-local Tab2 = Window:NewTab("Oyuncu")
-local Section2 = Tab2:NewSection("Karakter Ayarları")
+local Tab2 = Window:NewTab("Ayarlar")
+local Section2 = Tab2:NewSection("Menü Kontrolü")
 
--- Steal A Brainrot (Chilli Hub yüklenecek şekilde ayarlandı)
-Section1:NewButton("Steal A Brainrot (Chilli)", "Chilli Hub Scriptini Yukler", function()
+-- Oyun Butonları
+Section1:NewButton("Steal A Brainrot (Chilli)", "Chilli Hub Yukler", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ChilliHacks/ChilliHub/main/Source"))()
 end)
 
--- MM2 (Murder Mystery 2)
 Section1:NewButton("Vynixu (MM2)", "Esp ve Sheriff Aim", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Vynixu/VynixuLib/main/MM2/Source.lua"))()
 end)
 
--- Blox Fruits
 Section1:NewButton("Redz Hub (Blox Fruits)", "Auto Farm", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/REDZHUB/BloxFruits/main/redz7.lua"))()
 end)
 
--- Brookhaven
 Section1:NewButton("Chaos Hub (Brookhaven)", "Brookhaven hilesi", function()
     loadstring(game:HttpGet("https://rawscripts.net/raw/Brookhaven-RP-Chaos-Hub-31051"))()
 end)
 
--- Oyuncu Özellikleri
-Section2:NewSlider("Hız (WalkSpeed)", "Hızını Ayarlar", 500, 16, function(s)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+-- AÇ/KAPAT (TOGGLE) BUTONU
+-- Bu buton menüyü silmez, sadece görünmez yapar.
+Section2:NewKeybind("Menü Kapat/Aç (Keybind)", "Seçtiğin tuşla menüyü gizler", Enum.KeyCode.F, function()
+	Library:ToggleUI()
 end)
 
-Section2:NewSlider("Zıplama (JumpPower)", "Zıplama Gücü", 500, 50, function(s)
-    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+Section2:NewButton("Menüyü Gizle", "Menüyü ekranından saklar", function()
+    Library:ToggleUI()
+end)
+
+-- Hız Ayarı
+Section2:NewSlider("Hız (WalkSpeed)", "Hızını Ayarlar", 500, 16, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)

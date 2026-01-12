f9d92d3f6323c11b239ab9a03b9a9d7b82557cf9
dirt145432-del <ayse.tms21@gmail.com>
2026-01-12 09:30:36 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,33 +1,28 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Nothe Hub", "DarkTheme")
 
-local Tab = Window:NewTab("Scriptler")
-local Section = Tab:NewSection("Genel Scriptler")
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Nothe Hub", "DarkTheme")
-
--- Ana Sekmeler
+-- Sekmeler
 local Tab1 = Window:NewTab("Script Listesi")
-local Section1 = Tab1:NewSection("Hazır Scriptler")
+local Section1 = Tab1:NewSection("Brookhaven & Genel")
 
-local Tab2 = Window:NewTab("Oyuncu Özellikleri")
+local Tab2 = Window:NewTab("Oyuncu")
 local Section2 = Tab2:NewSection("Karakter Ayarları")
 
--- Script Butonları
-Section1:NewButton("Chilli Hub", "Scripti Yükler", function()
+-- Chaos Hub Butonu
+Section1:NewButton("Chaos Hub (Brookhaven)", "Chaos Hub Scriptini Yukler", function()
+    loadstring(game:HttpGet("https://rawscripts.net/raw/Brookhaven-RP-Chaos-Hub-31051"))()
+end)
+
+-- Chilli Hub Butonu
+Section1:NewButton("Chilli Hub", "Chilli Hub Scriptini Yukler", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ChilliHacks/ChilliHub/main/Source"))()
 end)
 
--- Fly (Uçma) Butonu
-Section2:NewButton("Fly (Uçma Modu)", "Karakteri Uçurur", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.lua"))()
-end)
-
--- Hız Ayarı
-Section2:NewSlider("Yürüme Hızı", "Hızınızı Ayarlar", 500, 16, function(s)
+-- Oyuncu Özellikleri (Sliderlar)
+Section2:NewSlider("Hız (WalkSpeed)", "Hizini Ayarlar", 500, 16, function(s)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
-Section:NewButton("Chilli Hub", "Scripti Calistirir", function()
-    loadstring(game:HttpGet("https://pastefy.app/oxlwJTPr/raw"))()
+Section2:NewSlider("Zıplama (JumpPower)", "Ziplama Gucunu Ayarlar", 500, 50, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
 end)

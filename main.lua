907ac845ddf7894bdfa76d2176e917607b5dd686
dirt145432-del <ayse.tms21@gmail.com>
2026-01-12 09:12:44 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Nothe Hub", "DarkTheme")
+
+local Tab = Window:NewTab("Scriptler")
+local Section = Tab:NewSection("Genel Scriptler")
+
+Section:NewButton("Chilli Hub", "Scripti Calistirir", function()
+    loadstring(game:HttpGet("https://pastefy.app/oxlwJTPr/raw"))()
+end)
